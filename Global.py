a78f1294cc867cecb6f06e38ba679d36480cac70
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-06-24 16:17:15 +0200

    ajouté crédits

--- Global.py
+++ Global.py
@@ -4,6 +4,7 @@ Contient les variables et fonctions nécessaires dans plusieurs fichiers
 from sys import platform # connaitre la plateforme/OS
 __version__ = "1.0"
 __author__ = "Merlet Raphaël"
+__advisor__ = "Elisa"
 DefaultLabel = ["taskID", "userID", "name",
                              "date", "priority", "status", "tag"]
 # taille fenêtre (utilisé dans Tk.geometry())
@@ -16,7 +17,7 @@ def ShowVersion():
     """
     print("===============================================================")
     print(f"Productivity App v{__version__}")
-    print(f"Made by {__author__}")
+    print(f"Made by {__author__} (with the advices of {__advisor__})")
     print("Source : https://github.com/Ilade-s/productivite-app-TkVer")
     print("Server (optionnal) : https://github.com/Tifiloow/productivite-app")
     print("===============================================================")
